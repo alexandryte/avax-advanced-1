e83f6e8de2a8daf6ce16657336b03042fedc9c19
alexandryte <75197322+alexandryte@users.noreply.github.com>
2023-11-07 23:58:00 +0800

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "jewel"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "JewelCoin"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "JC"
 )
 
 var ID ids.ID
